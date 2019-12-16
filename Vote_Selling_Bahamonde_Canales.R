398728f3614e85bf85b8c44b33df113a1f7494cd
Hector Bahamonde <hector.bahamonde@rutgers.edu>
2019-12-16 16:17:42 -0300

    wip

--- Vote_Selling_Bahamonde_Canales.R
+++ Vote_Selling_Bahamonde_Canales.R
@@ -5,9 +5,26 @@ cat("\014")
 rm(list=ls())
 
 
+
 # Load the data
 if (!require("pacman")) install.packages("pacman"); library(pacman) 
 p_load(readxl)
 
-dat1 <- read_excel("/Users/hectorbahamonde/RU/research/Economic_Experiment_Vote_Selling/data/120619.xls")
+setwd("./Economic_Experiment_Vote_Selling")
+dir = getwd() # set dir
+setwd(dir) # set dir
+dat <-read_excel(paste(dir, "/data/Edit/dat.xlsx", sep="")) # read data
+
+
+
+
+
+
+
+############################## 
+# Ideological Distance and Vote-Buying/Selling
+##############################
+
+# VD: sold?
+# VI: rango [Distancia Votante Partido A], [Distancia Votante Partido B]
 
